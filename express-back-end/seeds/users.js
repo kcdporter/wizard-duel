b8431cabb1ad63f46bee9877d2c9ef4105ede770
kcdporter <kiracdporter@gmail.com>
2019-05-08 10:41:08 -0700

    abandoning branch for timeline

--- express-back-end/seeds/users.js
+++ express-back-end/seeds/users.js
@@ -6,9 +6,9 @@ exports.seed = function(knex, Promise) {
       return knex('users').insert([
         {id: 1, username: 'sebastian', email: 'seb@seb.ca', password:'', experience_points:800, wins: 0, losses:0 },
 
-        {id: 2, username: 'ronan', email: 'ronan@ronan.ca', password:'lighthouse', experience_points:0, wins: 1, losses:1 },
+        {id: 2, username: 'ronan', email: 'ronan@ronan.ca', password:'lighthouse', experience_points:100, wins: 1, losses:1 },
 
-        {id: 3, username: 'kira', email: 'kira@kira.ca', password:'Yukon', experience_points:0, wins: 2, losses:2 },
+        {id: 3, username: 'kira', email: 'kira@kira.ca', password:'Yukon', experience_points:1000, wins: 2, losses:2 },
       ]);
     });
 };
